5a42371e60bf385a2591974511dd3bc34eb69eec
Boni Garcia <bgarcia@gsyc.es>
2019-03-21 17:41:51 +0100

    Update preprocess script

--- scripts/preprocess_videos.sh
+++ scripts/preprocess_videos.sh
@@ -1,33 +1,61 @@
-#!/bin/sh
+#!/bin/bash
 
 FPS=24
-PRESENTER=presenter.webm
-VIEWER=viewer.webm
-TMP_PRESENTER=p.webm
-TMP_VIEWER=v.webm
-CUT_PRESENTER=p-cut.webm
-CUT_VIEWER=v-cut.webm
-YUV_PRESENTER=p.yuv
-YUV_VIEWER=v.yuv
-TARGET_FODLER_YUV=.
+PREFFIX=0
+PRESENTER=$PREFFIX-presenter.webm
+VIEWER=$PREFFIX-viewer.webm
+TMP_PRESENTER=$PREFFIX-p.webm
+TMP_VIEWER=$PREFFIX-v.webm
+CUT_PRESENTER=$PREFFIX-p-cut.webm
+CUT_VIEWER=$PREFFIX-v-cut.webm
+YUV_PRESENTER=$PREFFIX-p.yuv
+YUV_VIEWER=$PREFFIX-v.yuv
+JPG_FOLDER=jpg
+SOURCE_FOLDER=.
+
+duration() {
+    num=$1
+    decimals=
+
+    strindex $1 .
+    if [ $retval -gt 0 ]; then
+        num=$(echo $1 | cut -d'.' -f 1)
+        decimals=$(echo $1 | cut -d'.' -f 2)
+    fi
+
+    ((h=num/3600))
+    ((m=num%3600/60))
+    ((s=num%60))
+
+    retval=$(printf "%02d:%02d:%02d\n" $h $m $s)
+
+    if [ ! -z "$decimals" ]; then
+        retval="${retval}.${decimals}"
+    fi
+}
+
+strindex() {
+  x="${1%%$2*}"
+  retval=$([[ "$x" = "$1" ]] && echo -1 || echo "${#x}")
+}
 
 
 # 1. Copying presenter and viewer files to current folder
-if [ ! -f ../$PRESENTER ]; then
-    echo ../$PRESENTER does not exist
+if [ ! -f $SOURCE_FOLDER/$PRESENTER ]; then
+    echo $SOURCE_FOLDER/$PRESENTER does not exist
     exit 1
 fi
-if [ ! -f ../$VIEWER ]; then
-    echo ../$VIEWER does not exist
+if [ ! -f $SOURCE_FOLDER/$VIEWER ]; then
+    echo $SOURCE_FOLDER/$VIEWER does not exist
     exit 1
 fi
 if [ ! -f $PRESENTER ]; then
     echo Copying $PRESENTER
-    cp ../$PRESENTER .
+    cp $SOURCE_FOLDER/$PRESENTER .
 fi
 if [ ! -f $VIEWER ]; then
     echo Copying $VIEWER
-    cp ../$VIEWER .
+    cp $SOURCE_FOLDER/$VIEWER .
 fi
 
 # 2. Remux presenter and viewer with a fixed bitrate
@@ -45,34 +73,55 @@ fi
 
 # 3. Trim presenter and viewer (remove paddings)
 # Extract images per frame (to find out change from padding to video and viceversa)
-#ffmpeg -i $TMP_PRESENTER p_%04d.jpg
-CUT_PRESENTER_FRAME_FROM=29
-CUT_PRESENTER_FRAME_TO=211
+
+# Uncomment this line to find out frames (from, to) and stop script
+# ffmpeg -i $TMP_PRESENTER $SOURCE_FOLDER/$JPG_FOLDER/p_%04d.jpg && exit 0
+
+CUT_PRESENTER_FRAME_FROM=146
+CUT_PRESENTER_FRAME_TO=1940
 CUT_PRESENTER_TIME_FROM=$(jq -n $CUT_PRESENTER_FRAME_FROM/$FPS)
 CUT_PRESENTER_TIME_TO=$(jq -n $CUT_PRESENTER_FRAME_TO/$FPS)
 CUT_PRESENTER_TIME=$(jq -n $CUT_PRESENTER_TIME_TO-$CUT_PRESENTER_TIME_FROM)
 
 if [ ! -f $CUT_PRESENTER ]; then
     echo Cutting presentr
-    ffmpeg -i $TMP_PRESENTER -ss 00:00:0$CUT_PRESENTER_TIME_FROM -t 00:00:0$CUT_PRESENTER_TIME -c:v libvpx -c:a libvorbis -y $CUT_PRESENTER
+    duration $CUT_PRESENTER_TIME_FROM
+    from=$retval
+    duration $CUT_PRESENTER_TIME
+    to=$retval
+    ffmpeg -i $TMP_PRESENTER -ss $from -t $to -c:v libvpx -c:a libvorbis -y $CUT_PRESENTER
 fi
 #mediainfo $CUT_PRESENTER
 
+# Uncomment this line to find out frames (from, to) and stop script
+# ffmpeg -i $TMP_VIEWER $SOURCE_FOLDER/$JPG_FOLDER/v_%04d.jpg && exit 0
 
-#ffmpeg -i $TMP_VIEWER v_%04d.jpg
-CUT_VIEWER_FRAME_FROM=29
-CUT_VIEWER_FRAME_TO=211
+CUT_VIEWER_FRAME_FROM=146
+CUT_VIEWER_FRAME_TO=1940
 CUT_VIEWER_TIME_FROM=$(jq -n $CUT_VIEWER_FRAME_FROM/$FPS)
 CUT_VIEWER_TIME_TO=$(jq -n $CUT_VIEWER_FRAME_TO/$FPS)
 CUT_VIEWER_TIME=$(jq -n $CUT_VIEWER_TIME_TO-$CUT_VIEWER_TIME_FROM)
 
 if [ ! -f $CUT_VIEWER ]; then
     echo Cutting presentr
-    ffmpeg -i $TMP_VIEWER -ss 00:00:0$CUT_VIEWER_TIME_FROM -t 00:00:0$CUT_VIEWER_TIME -c:v libvpx -c:a libvorbis -y $CUT_VIEWER
+    duration $CUT_VIEWER_TIME_FROM
+    from=$retval
+    duration $CUT_VIEWER_TIME
+    to=$retval
+    ffmpeg -i $TMP_VIEWER -ss $from -t $to -c:v libvpx -c:a libvorbis -y $CUT_VIEWER
 fi
 #mediainfo $CUT_VIEWER
 
 # 4. Convert videos to yuv420p
-ffmpeg -i $CUT_PRESENTER -pix_fmt yuv420p -c:v rawvideo -an -y $TARGET_FODLER_YUV/$YUV_PRESENTER
-ffmpeg -i $CUT_VIEWER -pix_fmt yuv420p -c:v rawvideo -an -y $TARGET_FODLER_YUV/$YUV_VIEWER
+if [ ! -f $YUV_PRESENTER ]; then
+	ffmpeg -i $CUT_PRESENTER -pix_fmt yuv420p -c:v rawvideo -an -y $YUV_PRESENTER
+fi
+
+if [ ! -f $YUV_VIEWER ]; then
+	ffmpeg -i $CUT_VIEWER -pix_fmt yuv420p -c:v rawvideo -an -y $YUV_VIEWER
+fi
+
+# 5. Run VMAF
+echo "Run VMAF with the following command:"
+echo ./run_vmaf yuv420p 540 360 "$PWD"/"$YUV_PRESENTER" "$PWD"/"$YUV_VIEWER" --out-fmt json ">" "$PWD"/"$PREFFIX"-vmaf.json
 
