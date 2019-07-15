b38c26884e82c0033a5d879457b4c959dbe49bd6
Boni Garcia <bgarcia@gsyc.es>
2019-07-15 17:04:31 +0200

    Automatic padding video check

--- scripts/calculate_qoe_metrics.sh
+++ scripts/calculate_qoe_metrics.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 ####################################################################################
-# Change these values manually according to the frames in which the actual video
-# (without the padding part, i.e. the lavfi video)
-####################################################################################
-DEFAULT_PREFIX=20
-CUT_PRESENTER_FRAME_FROM=117
-CUT_PRESENTER_FRAME_TO=1556
-CUT_VIEWER_FRAME_FROM=112
-CUT_VIEWER_FRAME_TO=1548
-####################################################################################
-
-####################################################################################
 # Fill these values with the binary path to VMAF and VQMT (compile from sources at
 # https://github.com/Netflix/vmaf and https://github.com/Rolinh/VQMT)
 ####################################################################################
@@ -19,6 +8,7 @@ VMAF_PATH=
 VQMT_PATH=
 ####################################################################################
 
+DEFAULT_PREFIX=20
 PREFIX=${1:-$DEFAULT_PREFIX}
 SOURCE_FOLDER=..
 FPS=24
@@ -62,6 +52,156 @@ strindex() {
   retval=$([[ "$x" = "$1" ]] && echo -1 || echo "${#x}")
 }
 
+find_index() {
+    x="${1%%$2*}"
+    retval=${#x}
+}
+
+get_r() {
+   input=$1
+   find_index $input "$rgb("
+   i=$retval
+   str=${input:i}
+   find_index $str ","
+   j=$retval
+   retval=$(echo $str | cut -c2-$j)
+}
+
+
+get_g() {
+   input=$1
+   find_index $input ","
+   i=$(($retval + 1))
+   str=${input:i}
+   find_index $str ","
+   j=$retval
+   retval=$(echo $str | cut -c1-$j)
+}
+
+get_b() {
+   input=$1
+   find_index $input ","
+   i=$(($retval + 1))
+   str=${input:i}
+   find_index $str ","
+   i=$(($retval + 1))
+   str=${str:i}
+   find_index $str ","
+   j=$(($retval - 1))
+   retval=$(echo $str | cut -c1-$j)
+}
+
+get_rgb() {
+   image=$1
+   width=$2
+   height=$3
+   retval=$(convert $image -format "%[pixel:u.p{$width,$height}]" -colorspace rgb info:)
+}
+
+match_threshold() {
+   input=$1
+   expected=$2
+   threshold=$3
+   diff=$((input-expected))
+   absdiff=${diff#-}
+
+   #echo "$input==$expected -> $absdiff<=$threshold"
+
+   if [ "$absdiff" -le "$threshold" ]; then
+      retval=true
+   else
+      retval=false
+   fi
+}
+
+match_rgb() {
+   r=$1
+   g=$2
+   b=$3
+   exp_r=$4
+   exp_g=$5
+   exp_b=$6
+
+   match_threshold $r $exp_r $threshold
+   match_r=$retval
+   match_threshold $g $exp_g $threshold
+   match_g=$retval
+   match_threshold $b $exp_b $threshold
+   match_b=$retval
+
+   #echo "$r==$exp_r->$match_r $g==$exp_g->$match_g $b==$exp_b->$match_b"
+
+   if $match_r && $match_g && $match_b; then
+      retval=true
+   else
+      retval=false
+   fi
+}
+
+
+match_color() {
+   image=$1
+   width=$2
+   height=$3
+   threshold=$4
+   expected_r=$5
+   expected_g=$6
+   expected_b=$7
+
+   #echo "match_color($1): $2,$3 -- $5 $6 $7"
+
+   get_rgb $image $width $height
+   color=$retval
+   get_r "$color"
+   r=$retval
+   get_g "$color"
+   g=$retval
+   get_b "$color"
+   b=$retval
+
+   match_rgb $r $g $b $expected_r $expected_g $expected_b
+}
+
+# Expected values for lavfi padding video: width,height rgb(r,g,b) [color]:
+# 60,240  rgb(255,255,255) [white]
+# 120,240 rgb(0,255,255)  [cyan]
+# 200,240 rgb(255,0,253) [purple]
+# 280,240 rgb(0,0,253) [blue]
+# 360,240 rgb(253,255,0) [yellow]
+# 420,240 rgb(0,255,0) [green]
+# 500,240 rgb(253,0,0) [red]
+# 580,240 rgb(0,0,0) [black]
+match_image() {
+   image=$1
+   threshold=50
+   height=240
+
+   match_color $image 60 $height $threshold 255 255 255
+   match_white=$retval
+   match_color $image 120 $height $threshold 0 255 255
+   match_cyan=$retval
+   match_color $image 200 $height $threshold 255 0 255
+   match_purple=$retval
+   match_color $image 280 $height $threshold 0 0 255
+   match_blue=$retval
+   match_color $image 360 $height $threshold 255 255 0
+   match_yellow=$retval
+   match_color $image 420 $height $threshold 0 255 0
+   match_green=$retval
+   match_color $image 500 $height $threshold 255 0 0
+   match_red=$retval
+   match_color $image 580 $height $threshold 0 0 0
+   match_black=$retval
+
+   if $match_black && $match_cyan && $match_purple && $match_blue && $match_yellow && $match_red && $match_black; then
+      retval=true
+   else
+      retval=false
+   fi
+}
+
+####################################################################################
+####################################################################################
 
 # 0. Check VMAF and VQMT path
 if [ -z "$VMAF_PATH" ]; then
@@ -108,46 +248,85 @@ fi
 
 mkdir -p $JPG_FOLDER
 
-CUT_PRESENTER_TIME_FROM=$(jq -n $CUT_PRESENTER_FRAME_FROM/$FPS)
-CUT_PRESENTER_TIME_TO=$(jq -n $CUT_PRESENTER_FRAME_TO/$FPS)
-CUT_PRESENTER_TIME=$(jq -n $CUT_PRESENTER_TIME_TO-$CUT_PRESENTER_TIME_FROM)
+if [ ! -f $CUT_PRESENTER ]; then
+    p_suffix="-p.jpg"
+    ffmpeg -i $TMP_PRESENTER $JPG_FOLDER/%04d$p_suffix
+    jpgs=("$JPG_FOLDER/*$p_suffix")
+    i_jpgs=$(ls -r $jpgs)
 
-if [ "$(ls -A $JPG_FOLDER)" ]; then
-    if [ ! -f $CUT_PRESENTER ]; then
-        rm $JPG_FOLDER/*.jpg
-        echo Cutting presenter
-        duration $CUT_PRESENTER_TIME_FROM
-        from=$retval
-        duration $CUT_PRESENTER_TIME
-        to=$retval
-        ffmpeg -i $TMP_PRESENTER -ss $from -t $to -c:v libvpx -c:a libvorbis -y $CUT_PRESENTER
-    fi
-else
-    ffmpeg -i $TMP_PRESENTER $JPG_FOLDER/%04d-p.jpg
-    echo "*** Check $JPG_FOLDER folder and inspect frame in which media starts and end ***"
-    echo "*** With that info, fill the variable CUT_PRESENTER_TIME_FROM and CUT_PRESENTER_FRAME_TO ***" 
-    exit 0
+    echo "Checking padding in presenter video ... please wait"
+    for i in $jpgs; do
+        file=$(echo $i)
+        match_image "$file"
+        match=$retval
+        if ! $match; then
+           CUT_PRESENTER_FRAME_FROM=$(echo "$file" | tr -dc '0-9')
+           break
+        fi
+    done
+
+    for i in $i_jpgs; do
+        file=$(echo $i)
+        match_image "$file"
+        match=$retval
+        if ! $match; then
+           CUT_PRESENTER_FRAME_TO=$(echo "$file" | tr -dc '0-9')
+           break
+        fi
+    done
+
+    echo "*** Cutting presenter from frame $CUT_PRESENTER_FRAME_FROM to $CUT_PRESENTER_FRAME_TO"
+
+    CUT_PRESENTER_TIME_FROM=$(jq -n $CUT_PRESENTER_FRAME_FROM/$FPS)
+    CUT_PRESENTER_TIME_TO=$(jq -n $CUT_PRESENTER_FRAME_TO/$FPS)
+    CUT_PRESENTER_TIME=$(jq -n $CUT_PRESENTER_TIME_TO-$CUT_PRESENTER_TIME_FROM)
+
+    duration $CUT_PRESENTER_TIME_FROM
+    from=$retval
+    duration $CUT_PRESENTER_TIME
+    to=$retval
+    ffmpeg -i $TMP_PRESENTER -ss $from -t $to -c:v libvpx -c:a libvorbis -y $CUT_PRESENTER
 fi
 
-CUT_VIEWER_TIME_FROM=$(jq -n $CUT_VIEWER_FRAME_FROM/$FPS)
-CUT_VIEWER_TIME_TO=$(jq -n $CUT_VIEWER_FRAME_TO/$FPS)
-CUT_VIEWER_TIME=$(jq -n $CUT_VIEWER_TIME_TO-$CUT_VIEWER_TIME_FROM)
 
-if [ "$(ls -A $JPG_FOLDER)" ]; then
-    if [ ! -f $CUT_VIEWER ]; then
-        rm $JPG_FOLDER/*.jpg
-        echo Cutting viewer
-        duration $CUT_VIEWER_TIME_FROM
-        from=$retval
-        duration $CUT_VIEWER_TIME
-        to=$retval
-        ffmpeg -i $TMP_VIEWER -ss $from -t $to -c:v libvpx -c:a libvorbis -y -max_muxing_queue_size 1024 $CUT_VIEWER
-    fi
-else
-    ffmpeg -i $TMP_VIEWER $JPG_FOLDER/%04d-v.jpg
-    echo "*** Check $JPG_FOLDER folder and inspect frame in which media starts and end ***"
-    echo "*** With that info, fill the variable CUT_VIEWER_FRAME_FROM and CUT_VIEWER_FRAME_TO ***" 
-    exit 0
+if [ ! -f $CUT_VIEWER ]; then
+    v_suffix="-v.jpg"
+    ffmpeg -i $TMP_VIEWER $JPG_FOLDER/%04d$v_suffix
+    jpgs=("$JPG_FOLDER/*$v_suffix")
+    i_jpgs=$(ls -r $jpgs)
+
+    echo "Checking padding in viewer video ... please wait"
+    for i in $jpgs; do
+        file=$(echo $i)
+        match_image "$file"
+        match=$retval
+        if ! $match; then
+           CUT_VIEWER_FRAME_FROM=$(echo "$file" | tr -dc '0-9')
+           break
+        fi
+    done
+
+    for i in $i_jpgs; do
+        file=$(echo $i)
+        match_image "$file"
+        match=$retval
+        if ! $match; then
+           CUT_VIEWER_FRAME_TO=$(echo "$file" | tr -dc '0-9')
+           break
+        fi
+    done
+
+    echo "*** Cutting viewer from frame $CUT_VIEWER_FRAME_FROM to $CUT_VIEWER_FRAME_TO"
+
+    CUT_VIEWER_TIME_FROM=$(jq -n $CUT_VIEWER_FRAME_FROM/$FPS)
+    CUT_VIEWER_TIME_TO=$(jq -n $CUT_VIEWER_FRAME_TO/$FPS)
+    CUT_VIEWER_TIME=$(jq -n $CUT_VIEWER_TIME_TO-$CUT_VIEWER_TIME_FROM)
+
+    duration $CUT_VIEWER_TIME_FROM
+    from=$retval
+    duration $CUT_VIEWER_TIME
+    to=$retval
+    ffmpeg -i $TMP_VIEWER -ss $from -t $to -c:v libvpx -c:a libvorbis -y -max_muxing_queue_size 1024 $CUT_VIEWER
 fi
 
 # 4. Convert videos to yuv420p
