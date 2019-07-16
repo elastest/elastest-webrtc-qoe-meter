812236d22dc3134e75adc346239395cab93fe2f6
Boni Garcia <bgarcia@gsyc.es>
2019-07-16 10:38:16 +0200

    Improve generate video input log messages

--- scripts/generate_input_video.sh
+++ scripts/generate_input_video.sh
@@ -7,31 +7,37 @@ PADDING_DURATION_SEC=5
 WIDTH=640
 HEIGHT=480
 FPS=24
+FFMPEG_LOG="-loglevel panic"
+TARGET_VIDEO=../test.y4m
 
 # 1. Download video sample from  https://archive.org/details/e-dv548_lwe08_christa_casebeer_003.ogg
 
 if [ ! -f $VIDEO_SAMPLE_NAME ]; then
-    echo $VIDEO_SAMPLE_NAME not exits ... downloading
+    echo "Content video ($VIDEO_SAMPLE_NAME) not exits ... downloading"
     wget $VIDEO_SAMPLE_URL
 else
-    echo $VIDEO_SAMPLE_NAME already exits
+    echo "Content video ($VIDEO_SAMPLE_NAME) already exits"
 fi
 
 # 2. Cut original video
-ffmpeg -i e-dv548_lwe08_christa_casebeer_003.mp4 -ss 00:00:00 -t $VIDEO_DURATION -vf scale="$WIDTH:$HEIGHT",setsar=1:1 -r $FPS tmp.mp4
+echo "Cutting original video (duration $VIDEO_DURATION)"
+ffmpeg $FFMPEG_LOG -i e-dv548_lwe08_christa_casebeer_003.mp4 -ss 00:00:00 -t $VIDEO_DURATION -vf scale="$WIDTH:$HEIGHT",setsar=1:1 -r $FPS tmp.mp4
 
 
 # 3. Create padding video based on a test pattern
-ffmpeg -f lavfi -i testsrc=duration=$PADDING_DURATION_SEC:size="$WIDTH"x"$HEIGHT":rate=$FPS padding.mp4
+echo "Creating padding video ($PADDING_DURATION_SEC seconds)"
+ffmpeg $FFMPEG_LOG -f lavfi -i testsrc=duration=$PADDING_DURATION_SEC:size="$WIDTH"x"$HEIGHT":rate=$FPS padding.mp4
 
 
-# 4. Concat final video
-ffmpeg -i padding.mp4 -i tmp.mp4 -i padding.mp4 -filter_complex concat=n=3 test.mp4
+# 4. Concatenate final video
+echo "Concatenating padding and content videos"
+ffmpeg $FFMPEG_LOG -i padding.mp4 -i tmp.mp4 -i padding.mp4 -filter_complex concat=n=3 test.mp4
 
 
 # 5. Convert video to Y4M
-ffmpeg -i test.mp4 -pix_fmt yuv420p test.y4m
+echo "Converting video to Y4M ($TARGET_VIDEO)"
+ffmpeg $FFMPEG_LOG -i test.mp4 -pix_fmt yuv420p $TARGET_VIDEO
 
-# 6. Delete temporal video files and move result to root (test is going to look for the file there)
+# 6. Delete temporal video files
+echo "Deleting temporal files"
 rm tmp.mp4 padding.mp4 test.mp4
-mv test.y4m ..
