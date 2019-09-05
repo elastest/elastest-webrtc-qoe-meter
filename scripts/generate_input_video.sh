5162a14d4ef5c41cbff854d513786f7090712951
Boni Garcia <bgarcia@gsyc.es>
2019-09-05 16:38:51 +0200

    Include audio processing in generate script

--- scripts/generate_input_video.sh
+++ scripts/generate_input_video.sh
@@ -7,8 +7,12 @@ PADDING_DURATION_SEC=5
 WIDTH=640
 HEIGHT=480
 FPS=24
+TONE_FREQUENCY_HZ=1000
+AUDIO_SAMPLE_RATE_HZ=44100
+AUDIO_CHANNELS_NUMBER=2
 FFMPEG_LOG="-loglevel panic"
 TARGET_VIDEO=../test.y4m
+TARGET_AUDIO=../test.wav
 CLEANUP=true
 
 # 1. Download video sample from  https://archive.org/details/e-dv548_lwe08_christa_casebeer_003.ogg
@@ -27,19 +31,23 @@ ffmpeg $FFMPEG_LOG -i e-dv548_lwe08_christa_casebeer_003.mp4 -ss 00:00:00 -t $VI
 
 # 3. Create padding video based on a test pattern
 echo "Creating padding video ($PADDING_DURATION_SEC seconds)"
-ffmpeg $FFMPEG_LOG -f lavfi -i testsrc=duration=$PADDING_DURATION_SEC:size="$WIDTH"x"$HEIGHT":rate=$FPS padding.mp4
+ffmpeg $FFMPEG_LOG -f lavfi -i testsrc=duration=$PADDING_DURATION_SEC:size="$WIDTH"x"$HEIGHT":rate=$FPS -f lavfi -i sine=frequency=$TONE_FREQUENCY_HZ:duration=$PADDING_DURATION_SEC padding.mp4
 
 
 # 4. Concatenate final video
 echo "Concatenating padding and content videos"
-ffmpeg $FFMPEG_LOG -i padding.mp4 -i tmp.mp4 -i padding.mp4 -filter_complex concat=n=3 test.mp4
+ffmpeg $FFMPEG_LOG -i padding.mp4 -i tmp.mp4 -i padding.mp4 -filter_complex concat=n=3:v=1:a=1 test.mp4
 
 
 # 5. Convert video to Y4M
-echo "Converting video to Y4M ($TARGET_VIDEO)"
+echo "Converting resulting video to Y4M ($TARGET_VIDEO)"
 ffmpeg $FFMPEG_LOG -y -i test.mp4 -pix_fmt yuv420p $TARGET_VIDEO
 
-# 6. Delete temporal video files
+# 6. Convert audio to Y4M
+echo "Converting resulting audio to WAV ($TARGET_AUDIO)"
+ffmpeg $FFMPEG_LOG -y -i test.mp4 -vn -acodec pcm_s16le -ar $AUDIO_SAMPLE_RATE_HZ -ac $AUDIO_CHANNELS_NUMBER $TARGET_AUDIO
+
+# 7. Delete temporal video files
 if $CLEANUP; then
     echo "Deleting temporal files"
     rm tmp.mp4 padding.mp4 test.mp4
