1e2ba765fa305d8bbe56050e74e1dd9b11953ae4
Boni Garcia <bgarcia@gsyc.es>
2019-11-12 17:14:43 +0100

    Bump RecortRTC library to version to 5.5.8

--- src/main/java/io/elastest/webrtc/qoe/ElasTestRemoteControlParent.java
+++ src/main/java/io/elastest/webrtc/qoe/ElasTestRemoteControlParent.java
@@ -126,7 +126,7 @@ public class ElasTestRemoteControlParent {
     private void injectRecordRtc(WebDriver driver) {
         String recordingJs = "var recScript=window.document.createElement('script');";
         recordingJs += "recScript.type='text/javascript';";
-        recordingJs += "recScript.src='https://cdnjs.cloudflare.com/ajax/libs/RecordRTC/5.5.4/RecordRTC.min.js';";
+        recordingJs += "recScript.src='https://cdnjs.cloudflare.com/ajax/libs/RecordRTC/5.5.8/RecordRTC.js';";
         recordingJs += "window.document.head.appendChild(recScript);";
         recordingJs += "return true;";
         this.executeScript(driver, recordingJs);
